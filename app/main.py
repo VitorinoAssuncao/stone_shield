f6c2f5ee15d54f2505ebff9ddcac804dc905c364
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-21 00:38:21 -0300

    add characthers
    adicionada estrutura de personagens favoritos (model, resource e rotas)

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import falcon
 from app.database.manager import DBManager
 from app.resources.users import UsersCollection, UsersItem
 from app.resources.comics import ComicsCollection, ComicsUserCollection,ComicItem
+from app.resources.characthers import CharacthersCollection, CharacthersUserCollection, CharactherItem
 from app.middleware import HandleCORS
 
 api = falcon.API(middleware=[HandleCORS()])
@@ -15,11 +16,18 @@ comics = ComicsCollection(db)
 user_comics = ComicsUserCollection(db)
 comic = ComicItem(db)
 
+characthers = CharacthersCollection(db)
+user_characthers = CharacthersUserCollection(db)
+characther = CharactherItem(db)
+
 api.add_route('/users',users)
 api.add_route('/users/{id}',user)
+api.add_route('/users/{id}/comics',user_comics)
+api.add_route('/users/{id}/characthers',user_characthers)
 api.add_route('/comics',comics)
 api.add_route('/comics/{id}',comic)
-api.add_route('/users/{id}/comics',user_comics)
+api.add_route('/characther',characthers)
+api.add_route('/characther/{id}',characther)
 
 if __name__ == "__main__":
     from wsgiref import simple_server
---
+++ app/resources/characthers.py
@@ -0,0 +1,40 @@
+import falcon
+import json
+
+from app.database.models.characthers_models import  Characthers
+from app.resources import BaseResource
+
+from sqlalchemy.exc import IntegrityError,SQLAlchemyError
+
+class CharacthersCollection(BaseResource):
+    def on_get(self,req,resp):
+        model_list = Characthers.get_list(self.db.session)
+        obj = [char.serialize() for char in model_list]
+        resp.status = falcon.HTTP_200
+        resp.media = obj
+
+class CharacthersUserCollection(BaseResource):
+    def on_get(self,req,resp,id):
+        model_list = Characthers.get_all_chars_from_user(self.db.session,id)
+        obj = [char.serialize() for char in model_list]
+        resp.status = falcon.HTTP_200
+        resp.media = obj
+
+class CharactherItem(BaseResource):
+    def on_get(self,req,resp,id):
+        char_model = Characthers.get_char_by_id(self.db.session,id).serialize()
+        resp.status = falcon.HTTP_200
+        resp.media = char_model
+
+    def on_delete(self,req,resp,id):
+        try:
+            Characthers.delete_characther(self.db.session,id)
+
+        except SQLAlchemyError:
+            raise falcon.HTTPBadRequest(
+                'Não foi possivel deletar o personagem',
+                'Validar o código informado.'
+            )
+
+        resp.status = falcon.HTTP_200
+        resp.media = "Personagem removido com sucesso"
