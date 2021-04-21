edf13cdee3ba0a8ea9dd212a08f0a1ad35f649f9
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-21 02:35:07 -0300

    add rota de banco heroku
    adicionado dados para conectar ao banco quando hosteado via heroku

--- app/database/manager.py
+++ app/database/manager.py
@@ -12,10 +12,10 @@ class DBManager(object):
         # self.connection = 'postgresql+psycopg2://dev:dev@localhost:5432/cdd_controller'
         
         # Rota Local
-        self.connection = 'postgresql+psycopg2://postgres:R@posinh@1@localhost:5432/stone_marvel'
+        # self.connection = 'postgresql+psycopg2://postgres:R@posinh@1@localhost:5432/stone_marvel'
 
         # Rota configurada para a base do heroku
-        # self.connection = 'postgresql+psycopg2://nmonwnodnkydnb:137c1c8d1013754f50de8ce05e22b3f096c3d830ac64f417c2e7e67867d1afff@ec2-52-1-115-6.compute-1.amazonaws.com:5432/dbt159vt4r4duk'
+        self.connection = 'postgresql+psycopg2://ubvmhrvfbflvlm:8d786b202b4e1e9595c90293dfda1e0c7c2ea97732b5ce61d9f4c5b92952a16d@ec2-54-162-119-125.compute-1.amazonaws.com:5432/df4lb3onq4g9r0'
         
         self.engine = sqlalchemy.create_engine(self.connection)
         self.DBSession = scoping.scoped_session(
