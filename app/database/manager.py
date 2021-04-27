9d10ecbc3063c889b8e497933cd090276f94c825
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-27 19:31:10 -0300

    del itens antigos
    removidos itens de versão anteriores.

--- app/database/manager.py
+++ app/database/manager.py
@@ -8,12 +8,6 @@ from sqlalchemy.orm import scoping
 
 class DBManager(object):
     def __init__(self):
-        # Rota configurada para a base no docker           
-        # self.connection = 'postgresql+psycopg2://dev:dev@localhost:5432/cdd_controller'
-        
-        # Rota Local
-        # self.connection = 'postgresql+psycopg2://postgres:R@posinh@1@localhost:5432/stone_marvel'
-
         # Rota configurada para a base do heroku
         self.connection = 'postgresql+psycopg2://ubvmhrvfbflvlm:8d786b202b4e1e9595c90293dfda1e0c7c2ea97732b5ce61d9f4c5b92952a16d@ec2-54-162-119-125.compute-1.amazonaws.com:5432/df4lb3onq4g9r0'
         
